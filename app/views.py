cea0f5e04d3b13d95f78d1d1d168374c96fcb551
Paul Jovenn Sarmiento <jovennsarmiento2@gmail.com>
2023-10-17 22:02:52 +0800

    made webapp interface, as well as student table interface

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def index():
+    return render_template("index.html")
+
+@views.route('/students')
+def student():
+    return render_template("student.html")
+
+@views.route('/course')
+def course():
+    return render_template("course.html")
+
+@views.route('/college')
+def college():
+    return render_template("college.html")
