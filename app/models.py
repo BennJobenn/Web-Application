1d08bc768fd27361b64ab867d1bc40f55fb95843
Paul Jovenn Sarmiento <jovennsarmiento2@gmail.com>
2023-10-20 19:58:42 +0800

    Can now add student,course,college data

--- app/models.py
+++ app/models.py
@@ -1,6 +1,16 @@
 from app import mysql
 
 class studentmodel:
+    @classmethod
+    def create_student(cls, id, firstname, lastname, course_code, year, gender):
+        try:
+            cur = mysql.new_cursor(dictionary=True)
+            cur.execute("INSERT INTO student (id, firstname, lastname, course_code, year, gender) VALUES (%s, %s, %s, %s, %s, %s)",
+                        (id, firstname, lastname, course_code, year, gender))
+            mysql.connection.commit()
+            return "Student data added!"
+        except Exception as e:
+            return f"Adding student failed: {str(e)}"
 
     @classmethod
     def get_students(cls):
@@ -16,9 +26,9 @@ class collegemodel:
             cur = mysql.new_cursor(dictionary=True)
             cur.execute("INSERT INTO college (code, name) VALUES (%s, %s)", (code, name))
             mysql.connection.commit()
-            return "College created successfully"
+            return "College added to data"
         except Exception as e:
-            return f"Failed to create college: {str(e)}"
+            return f"Failed to add college: {str(e)}"
 
     @classmethod
     def get_colleges(cls):
@@ -26,6 +36,18 @@ class collegemodel:
         cur.execute("SELECT code, name FROM college")
         colleges = cur.fetchall()
         return colleges
+    
+    @classmethod
+    def delete_college(cls, code):
+        try:
+            cursor = mysql.connection.cursor(dictionary=True)
+            cursor.execute("DELETE FROM college WHERE code = %s", (code,))
+            mysql.connection.commit()
+            cursor.close()  # Close the cursor after the operation is complete
+            return "College deleted successfully"
+        except Exception as e:
+            return f"Failed to delete college: {str(e)}"
+
 
 class coursemodel:
     @classmethod
@@ -34,9 +56,9 @@ class coursemodel:
             cur = mysql.new_cursor(dictionary=True)
             cur.execute("INSERT INTO course (code, name, college_code) VALUES (%s, %s, %s)", (code, name, college_code))
             mysql.connection.commit()
-            return "Course created successfully"
+            return "Course added to data"
         except Exception as e:
-            return f"Failed to create course: {str(e)}"
+            return f"Failed to add course: {str(e)}"
     @classmethod
     def get_courses(cls):
         cur = mysql.new_cursor(dictionary=True)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 from app.models import studentmodel, coursemodel, collegemodel
 
 
@@ -10,17 +10,40 @@ def index():
 
 @views.route('/students', methods=['GET', 'POST'])
 def student():
+    if request.method == "POST":
+        id = request.form.get("inputID")
+        fname = request.form.get("inputFirstName")
+        lname = request.form.get("inputLastName")
+        course = request.form.get("inputCourse")
+        year = request.form.get("inputYear")
+        gender = request.form.get("inputGender")
+        studentmodel.create_student(id, fname, lname, course, year, gender)
+
+
     students = studentmodel.get_students()
     courses = coursemodel.get_courses()
     return render_template("student.html", students= students, courses=courses)
 
 @views.route('/course', methods=['GET', 'POST'])
 def course():
+    if request.method == "POST":
+        code = request.form.get("inputCOURSE_CD")
+        name = request.form.get("inputCOURSE_NM")
+        collegecode = request.form.get("inputCOURSE_CLG")
+        coursemodel.create_course(name, code, collegecode)
+
+
+    colleges = collegemodel.get_colleges()
     courses = coursemodel.get_courses()
-    return render_template("course.html", courses=courses)
+    return render_template("course.html", courses=courses, colleges=colleges)
 
 @views.route('/college', methods=['GET', 'POST'])
 def college():
+    if request.method == "POST":
+        code = request.form.get("inputCOLLEGE_CD")
+        name = request.form.get("inputCOLLEGE_NM")
+        collegemodel.create_college(name, code)
+
     colleges = collegemodel.get_colleges()
     return render_template("college.html", colleges=colleges)
 
@@ -34,3 +57,5 @@ def edit_course():
 @views.route('/editing_coll')
 def edit_college():
     return render_template("edit_college.html")
+
+
